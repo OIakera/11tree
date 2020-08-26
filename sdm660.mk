bf7def2343a36dbb1d57224083f89fe03d75999a
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-08-26 13:06:10 +0530

    sdm660-common: Import powerhint from sunfish
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -337,6 +337,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Protobuf
